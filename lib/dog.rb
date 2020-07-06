d7d767ff28119606a86ffe6781c72ce0437f7fde
William Han Findiesen <william.findiesen@gmail.com>
2020-07-06 03:10:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Dog
+  def name= (dog_name) # the setter
+    @this_dogs_name = dog_name
+  end
+  def name # the getter
+    @this_dogs_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
